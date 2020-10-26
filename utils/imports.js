e80822b141a157629bcc56c9cae827bb22b89087
Shree krishna Lamichhane <uniksk123@gmail.com>
2020-10-26 12:48:12 +0545

    ...

--- utils/imports.js
+++ utils/imports.js
@@ -9,10 +9,10 @@ const toJson = require("unsplash-js").toJson;
 const path = require('path');
 //Importing the databse models
 const parent = path.dirname(require.main.filename);
-let Image = require(parent+"\\models\\Image");
-const User = require(parent+"\\models\\User");
-const Keyword = require(parent+"\\models\\Keyword");
-const Variable = require(parent+"\\models\\Variables");
+let Image = require(parent+"/models/Image");
+const User = require(parent+"/models/User");
+const Keyword = require(parent+"/models/Keyword");
+const Variable = require(parent+"/models/Variables");
 // let Image = require(__dirname + "");
 // const User = require("../models/user");
 // const Keyword = require("../models/keyword");
@@ -20,6 +20,6 @@ const Variable = require(parent+"\\models\\Variables");
 // const Variable = require("../models/variables");
 
 //importing configurations
-const { unsplash_image } = require(parent +"\\config\\configuration");
+const { unsplash_image } = require(parent +"/config/configuration");
 
 module.exports = {fetch , Unsplash , toJson , Image , User , Keyword , Variable , unsplash_image}
